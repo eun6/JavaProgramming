d1e2cd7fda5b7664a52ae12fcf440adbef2f67d1
eun6 <yy.eun6@gmail.com>
2023-04-11 22:47:28 +0900

    메인 함수 내용 추가

--- Chapter5/src/pairProgramming/Num_14.java
+++ Chapter5/src/pairProgramming/Num_14.java
@@ -1,10 +1,26 @@
 package pairProgramming;
 
+interface Shape {
+	final double PI = 3.14;
+	void draw();
+	double getArea();
+	default public void redraw() {
+		System.out.println("--- 다시 그립니다. ");
+		draw();
+	}
+}
+
+
 public class Num_14 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Shape [] list = new Shape[3];
+		list[0] = new Circle(10);
+		list[1] = new Oval(20, 30);
+		list[2] = new Rect(10, 40);
+		
+		for(int i=0; i<list.length; i++) list[i].redraw();
+		for(int i=0; i<list.length; i++) System.out.println("면적은 " + list[i].getArea());
 	}
 
 }
